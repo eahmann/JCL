46f2f09743500a6d4849ae5f49665c0ec87ad3a2
Eric Ahmann <eric@ahmann.me>
2021-02-01 21:15:20 -0600

    Small adjustment to constants

--- src/main/java/com/awd/Constants.java
+++ src/main/java/com/awd/Constants.java
@@ -1,13 +1,18 @@
 package com.awd;
 
 public class Constants {
-    private Constants() {}
+    private Constants() {
+    }
+
     static final String VERSION = "v1";
     static final String APP_NAME = "Java Command Line";
     static final String WELCOME = "Welcome to %s %s\n";
     static final String COMMANDS_AVAIL = "Available commands: ";
-    static final String[] COMMANDS = {"LIST", "CHDIR", "RUN", "REMOVE", "RENAME", "HELP", "QUIT" };
+    static final String[] COMMANDS = {"LIST - Directory listing", "\nCHDIR - Change directory",
+            "\nRUN - Execute file", "\nREMOVE - delete file or folder", "\nRENAME - rename file or folder",
+            "\nHELP - display this message", "\nQUIT - exit program"};
     static final String INSTRUCT = "To begin enter command at prompt. Type HELP to see commands\n";
     static final String GOOD_BYE = "\nThank you for using %s %s\n";
     static final String CMD_EXEC = "COMMAND EXECUTED";
- }
+}
+
